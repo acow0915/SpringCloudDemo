7db869c58018d279a3c0fe494e2a7c37ee0892e0
Tim Lee <acow09151@gmail.com>
2017-11-01 23:18:17 +0800

    add config server

---
+++ ConfigServer/src/main/java/com/hystrix/dashboard/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class ConfigServerApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}

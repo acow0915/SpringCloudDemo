49304399198e959f68a646bd001eae165d5ff863
user <user@user-PC>
2017-11-02 17:07:49 +0800

    讓eureka-server 依賴 config-server的設定

--- ConfigServer/src/main/java/com/hystrix/dashboard/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/hystrix/dashboard/ConfigServerApplication.java
@@ -1,9 +1,11 @@
 package com.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringCloudApplication
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication 
 {
     public static void main( String[] args )
